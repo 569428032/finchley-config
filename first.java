64967a6b8582af6c6aeb4839736a775701b29ee1
x1ng <15138055163@163.com>
2018-09-17 10:45:56 +0800

    two

--- first.java
+++ first.java
@@ -1,4 +1,8 @@
-class 1{
+class first{
  public static void main(String[] args){
- 
+ System.out.println(Math.round(9.5));
+		System.out.println(Math.round(-9.5));
+		String string=new String("xyz");
+		System.out.println(string);
+	}1
 }
